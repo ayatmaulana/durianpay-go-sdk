c3972006cb36bd8ee59c93382c20a74bccc9d331
Ayat Maulana <ayatmaulana.04@gmail.com>
2022-06-21 02:17:08 +0700

    Import all modules to main

--- durianpay.go
+++ durianpay.go
@@ -1,22 +1,31 @@
 package durianpay
 
 import (
-  "github.com/ayatmaulana/durianpay-go-sdk/common"
+	"github.com/ayatmaulana/durianpay-go-sdk/common"
 
-  "github.com/ayatmaulana/durianpay-go-sdk/modules/disbursement"
-  "github.com/ayatmaulana/durianpay-go-sdk/modules/order"
+	"github.com/ayatmaulana/durianpay-go-sdk/modules/disbursement"
+	"github.com/ayatmaulana/durianpay-go-sdk/modules/order"
+	"github.com/ayatmaulana/durianpay-go-sdk/modules/payment"
+	"github.com/ayatmaulana/durianpay-go-sdk/modules/promo"
+	"github.com/ayatmaulana/durianpay-go-sdk/modules/settlement"
 )
 
 type Modules struct {
-  Disbursement disbursement.Disburesement
+  Disbursement disbursement.Disbursement
   Order order.Order
+  Payment payment.Payment
+  Promo promo.Promo
+  Settlement settlement.Settlement
 }
 
 func NewClient(config *common.ClientConfig) *Modules {
   agent := common.NewAgent(config)
 
   return &Modules{
-    Disbursement: disbursement.Disburesement{Agent: agent},
+    Disbursement: disbursement.Disbursement{Agent: agent},
     Order: order.Order{Agent: agent},
+    Payment: payment.Payment{Agent: agent},
+    Promo: promo.Promo{Agent: agent},
+    Settlement: settlement.Settlement{Agent: agent},
   }
 }
